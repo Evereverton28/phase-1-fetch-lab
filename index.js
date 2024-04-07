a8c78088e533069f67ff1b32d3df389205ec53b5
Everton <evertonkairu@gmail.com>
2024-04-07 17:18:17 +0300

    Lab complete

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 function fetchBooks() {
   return fetch("https://anapioficeandfire.com/api/books")
     .then((resp) => resp.json())
-    .then((json) => {
-      renderBooks(json); 
-      return json; 
-    });
+    .then((json) => renderBooks(json));
 }
 
 function renderBooks(books) {
